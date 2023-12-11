74164b48d8ee19d9cca38d4d018bd7dd81a9727b
kallebbraga <145399458+kallebbraga@users.noreply.github.com>
2023-12-10 23:58:07 -0500

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,10 @@
+// hello.js
+
+// Global variable
+var clickCount = 0;
+
+// Function to display a popup alert
+function hello() {
+    clickCount++;
+    alert("You clicked the button " + clickCount + " times");
+}
